0fa164e9f4aaeb3eafffc0cc8da369243c4e6c31
DTristan <tristan.diard@ynov.com>
2024-12-12 11:02:28 +0100

    finito

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 import re
 import utils
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('5.5.5.11', 13337))
+s.connect(('10.1.1.15', 13337))
 s.send('Hello'.encode())
 
 # On reçoit la string Hello
